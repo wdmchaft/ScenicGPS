76febd0910fc43c67359abce750daf4635f78323
Dan Lynch <dlynch@skatepark.local>
2011-03-08 15:41:36 -0800

    Added Geocoders as wells as UISegmentedControl to switch between Hybrid, Map, and Satellite views.

--- Scenic/ScenicMapViewController.h
+++ Scenic/ScenicMapViewController.h
@@ -6,15 +6,25 @@
 //  Copyright 2011 UC Berkeley. All rights reserved.
 //
 
+
+// thanks for the tips on this site for using geo coder and MKMap
+// http://blog.objectgraph.com/index.php/2009/04/03/iphone-sdk-30-playing-with-map-kit-part-2/
+
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import <MapKit/MKAnnotation.h>
+#import <MapKit/MKReverseGeocoder.h>
 
 
-
-@interface ScenicMapViewController : UIViewController <MKMapViewDelegate> {
-	MKMapView* _mapView;
+@interface ScenicMapViewController : UIViewController <MKReverseGeocoderDelegate,MKMapViewDelegate> {
+    MKMapView* _mapView;
+    MKReverseGeocoder *geoCoder;
+	MKPlacemark *mPlacemark;
+	UISegmentedControl *mapType;
     
 }
+
+- (void)changeType;
 
 @property (nonatomic, retain) MKMapView* mapView;
 
