3cde97284f9ebb1eaa5f9ea7397c00aa25a29de0
Jack Reilly <jackdreilly@gmail.com>
2011-03-30 15:03:23 -0700

    created helper class for all core data accesses

---
+++ Scenic/CDHelper.h
@@ -0,0 +1,25 @@
+//
+//  CDHelper.h
+//  Scenic
+//
+//  Created by Jack Reilly on 3/30/11.
+//  Copyright 2011 UC Berkeley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ScenicRoute;
+@interface CDHelper : NSObject {
+    
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+-(NSArray*) allRoutes;
+-(void) saveRoute: (ScenicRoute*) route;
++(id) sharedHelper;
+@end
