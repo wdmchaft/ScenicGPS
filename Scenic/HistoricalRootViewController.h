bd991ad52bc87e469afa461fea732d8e02caf207
Jack Reilly <jackdreilly@gmail.com>
2011-04-01 11:08:51 -0700

    made it a nav con

---
+++ Scenic/HistoricalRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HistoricalRootViewController.h
+//  Scenic
+//
+//  Created by Jack Reilly on 4/1/11.
+//  Copyright 2011 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HistoricalRootViewController : UINavigationController {
+    
+}
+
+@end
