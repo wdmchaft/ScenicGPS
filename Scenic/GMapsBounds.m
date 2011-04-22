181261a00ad0488133405317c95eb8a0871d3e41
Dan Lynch <dlynch@skatepark.local>
2011-04-22 16:20:05 -0700

    can now dynamically explore

--- Scenic/GMapsBounds.m
+++ Scenic/GMapsBounds.m
@@ -8,15 +8,35 @@
 
 #import "GMapsBounds.h"
 #import "GMapsCoordinate.h"
+#import "ScenicMapView.h"
 
 static NSString* NE_KEY = @"northeast";
 static NSString* SW_KEY = @"southwest";
 
 
-
 @implementation GMapsBounds
 @synthesize ne,sw;
 
+
++(id) boundsFromMapView:(ScenicMapView*) mMapView {
+    
+    CGPoint nePoint = CGPointMake(mMapView.bounds.origin.x + mMapView.bounds.size.width, mMapView.bounds.origin.y);
+   
+    CGPoint swPoint = CGPointMake((mMapView.bounds.origin.x), (mMapView.bounds.origin.y + mMapView.bounds.size.height));
+
+    CLLocationCoordinate2D neCoord;
+    neCoord = [mMapView convertPoint:nePoint toCoordinateFromView:mMapView];
+    CLLocationCoordinate2D swCoord;
+    swCoord = [mMapView convertPoint:swPoint toCoordinateFromView:mMapView];
+
+    
+    GMapsCoordinate * coord1 = [GMapsCoordinate coordFromCLCoord: neCoord];
+    GMapsCoordinate * coord2 = [GMapsCoordinate coordFromCLCoord: swCoord];
+    GMapsBounds * bounds = [[[GMapsBounds alloc] init] autorelease];
+    bounds.ne = coord1;
+    bounds.sw = coord2;
+    return bounds;
+}
 
 +(id) boundsFromJSONDic: (NSDictionary*) dic {
     GMapsBounds* bounds =  [[[GMapsBounds alloc] init] autorelease];
